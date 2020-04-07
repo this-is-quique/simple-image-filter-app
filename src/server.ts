782d01fd78a25db3237fd9ce29d8e6ac355f77d5
Quique <this_is_quique@protonmail.ch>
2020-04-07 23:11:21 +1000

    Add function that will handle the image and transformation and removal of image in tmp directory

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import fs from 'fs';
 import express from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
@@ -7,13 +8,26 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   const app = express();
 
   const port = process.env.PORT || 8082;
-  
+
   app.use(bodyParser.json());
 
   app.get( "/", async ( req, res ) => {
     res.send("try GET /filteredimage?image_url={{}}")
   } );
-  
+
+  app.get("/filteredimage", async ( req, res ) => {
+    const image_url:string = req.query.image_url;
+
+    if (!image_url) {
+      res.send("not image url provided")
+    }
+
+    const filteredImageLocalUrl:string = await filterImageFromURL(image_url);
+      res.sendFile(filteredImageLocalUrl, () => {
+        fs.unlinkSync(filteredImageLocalUrl)
+      });
+  });
+
   app.listen( port, () => {
       console.log( `server running http://localhost:${ port }` );
       console.log( `press CTRL+C to stop server` );
